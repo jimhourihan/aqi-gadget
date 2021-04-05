f4343956e91569c136ed6eb443b30520bdc90c8d
Jim Hourihan <jimh@Uzumaki.local>
2021-04-05 11:33:52 -0700

    Read-only fs
    make-usb-gadget: remount rw for updating hostname
    
    read-only fs

--- setup/pi-config-after-boot.sh
+++ setup/pi-config-after-boot.sh
@@ -1,13 +1,63 @@
 #!/bin/sh
+if [ $(id -u) -ne 0 ]; then
+	echo "Must be run as root."
+	echo "Try 'sudo bash $0'"
+	exit 1
+fi
+
+# Given a filename, a regex pattern to match and a replacement string:
+# Replace string if found, else no change.
+# (# $1 = filename, $2 = pattern to match, $3 = replacement)
+replace() {
+	grep $2 $1 >/dev/null
+	if [ $? -eq 0 ]; then
+		# Pattern found; replace in file
+		sed -i "s/$2/$3/g" $1 >/dev/null
+	fi
+}
+
+# Given a filename, a regex pattern to match and a replacement string:
+# If found, perform replacement, else append file w/replacement on new line.
+replaceAppend() {
+	grep $2 $1 >/dev/null
+	if [ $? -eq 0 ]; then
+		# Pattern found; replace in file
+		sed -i "s/$2/$3/g" $1 >/dev/null
+	else
+		# Not found; append on new line (silently)
+		echo $3 | sudo tee -a $1 >/dev/null
+	fi
+}
+
+# Given a filename, a regex pattern to match and a string:
+# If found, no change, else append file with string on new line.
+append1() {
+	grep $2 $1 >/dev/null
+	if [ $? -ne 0 ]; then
+		# Not found; append on new line (silently)
+		echo $3 | sudo tee -a $1 >/dev/null
+	fi
+}
+
+# Given a filename, a regex pattern to match and a string:
+# If found, no change, else append space + string to last line --
+# this is used for the single-line /boot/cmdline.txt file.
+append2() {
+	grep $2 $1 >/dev/null
+	if [ $? -ne 0 ]; then
+		# Not found; insert in file before EOF
+		sed -i "s/\'/ $3/g" $1 >/dev/null
+	fi
+}
 
 #raspi-config nonint do_change_hostname aqi-gadget
 apt update
 apt upgrade -y
 apt install dnsmasq -y
-# Do these if you're not using lite
-#apt-get remove --purge "libreoffice*" -y
-#apt-get purge wolfram-engine -y
-apt autoremove -y
+apt-get install busybox-syslogd -y
+apt-get remove --purge rsyslog -y
+apt-get remove -y --purge triggerhappy logrotate dphys-swapfile fake-hwclock
+apt-get -y autoremove --purge
 
 # move the 100MB image file into place and decompress it
 mv storage.fat32.dmg.gz /
@@ -49,40 +99,6 @@ denyinterfaces usb0
 noipv4ll
 EOF
 
-# create the (only) configuration
-mkdir configs/c.1 # dot and number mandatory
-
-# create the ethernet function
-mkdir functions/ecm.usb0 
-echo 32:71:15:18:ff:7a > functions/ecm.usb0/host_addr
-echo 32:71:15:18:ff:7b > functions/ecm.usb0/dev_addr
-
-# create mass_storage function
-mkdir functions/mass_storage.0
-echo 1 > functions/mass_storage.0/ro
-echo 1 > functions/mass_storage.0/removable
-echo /storage.fat32.dmg > functions/mass_storage.0/lun.0/file
-
-# assign function to configuration
-ln -s functions/ecm.usb0/ configs/c.1/
-ln -s functions/mass_storage.0/ configs/c.1/
-
-# bind!
-udevadm settle -t 5 || :
-ls /sys/class/udc > UDC 
-ifup usb0
-
-cd /home/pi/aqi-gadget
-./display_connection.py 
-
-ST=`cat /sys/devices/platform/soc/20980000.usb/udc/20980000.usb/state`
-if [ "$ST" = "not attached" ]; then
-    ifconfig usb0 down
-else
-    ifconfig wlan0 down
-fi
-EOF
-
 chmod +x /boot/make-usb-gadget
 
 cat > /etc/systemd/system/create-usb-gadgets.service <<EOF
@@ -110,15 +126,69 @@ apt install python3-numpy -y
 apt install libopenjp2-7-dev libtiff-dev -y
 pip3 install --upgrade --force-reinstall spidev
 pip3 install RPi.GPIO
-pip3 install serial setproctitle systemd 
+pip3 install wifi serial setproctitle systemd 
 pip3 install Adafruit-Blinka Adafruit-PlatformDetect Adafruit-PureIO
 pip3 install adafruit-circuitpython-bme680 adafruit-circuitpython-busdevice adafruit-circuitpython-pm25 adafruit-circuitpython-rgb-display
-pip3 install CherryPy
-pip3 install pillow numpy
+pip3 install CherryPy pillow numpy
 
 # ro sdcard setup
-apt-get remove --purge triggerhappy logrotate dphys-swapfile -y
-apt autoremove -y
+# Replace log management with busybox (use logread if needed)
+# Installing ntp and busybox-syslogd...
+apt-get -y --force-yes install ntp busybox-syslogd; dpkg --purge rsyslog
 
-# create the gadget and set the new hostname
-echo "Needs Reboot"
+# Add fastboot, noswap and/or ro to end of /boot/cmdline.txt
+append2 /boot/cmdline.txt fastboot fastboot
+append2 /boot/cmdline.txt noswap noswap
+append2 /boot/cmdline.txt ro^o^t ro
+
+# Move /var/spool to /tmp
+rm -rf /var/spool
+ln -s /tmp /var/spool
+
+# Make SSH work
+replaceAppend /etc/ssh/sshd_config "^.*UsePrivilegeSeparation.*$" "UsePrivilegeSeparation no"
+
+# Change spool permissions in var.conf (rondie/Margaret fix)
+replace /usr/lib/tmpfiles.d/var.conf "spool\s*0755" "spool 1777"
+
+# Move dhcpd.resolv.conf to tmpfs
+touch /tmp/dhcpcd.resolv.conf
+rm /etc/resolv.conf
+ln -s /tmp/dhcpcd.resolv.conf /etc/resolv.conf
+
+# Make edits to fstab
+# make / ro
+# tmpfs /var/log tmpfs nodev,nosuid 0 0
+# tmpfs /var/tmp tmpfs nodev,nosuid 0 0
+# tmpfs /tmp     tmpfs nodev,nosuid 0 0
+replace /etc/fstab "vfat\s*defaults\s" "vfat    defaults,ro "
+replace /etc/fstab "ext4\s*defaults,noatime\s" "ext4    defaults,noatime,ro "
+append1 /etc/fstab "/var/log" "tmpfs /var/log tmpfs nodev,nosuid 0 0"
+append1 /etc/fstab "/var/tmp" "tmpfs /var/tmp tmpfs nodev,nosuid 0 0"
+append1 /etc/fstab "\s/tmp"   "tmpfs /tmp    tmpfs nodev,nosuid 0 0"
+
+# random seed
+rm /var/lib/systemd/random-seed
+ln -s /tmp/random-seed /var/lib/systemd/random-seed
+cat >> /lib/systemd/system/systemd-random-seed.service <<EOF 
+ExecStartPre=/bin/echo "" >/tmp/random-seed
+EOF
+
+cat >> ~pi/.bashrc <<EOF
+set_bash_prompt() {
+    fs_mode=\$(mount | sed -n -e "s/^\/dev\/.* on \/ .*(\(r[w|o]\).*/\1/p")
+    PS1='\[\033[01;32m\]\u@\h\${fs_mode:+(\$fs_mode)}\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\] > '
+}
+alias ro='sudo mount -o remount,ro / ; sudo mount -o remount,ro /boot'
+alias rw='sudo mount -o remount,rw / ; sudo mount -o remount,rw /boot'
+alias start='sudo systemctl start aqi.service'
+alias stop='sudo systemctl stop aqi.service'
+alias status='sudo systemctl status aqi.service'
+PROMPT_COMMAND=set_bash_prompt
+EOF
+
+chown pi:pi ~pi/.bashrc
+
+echo "Set up complete -- file system will be read-only"
+echo "Edit aqi-gadget-info and reboot"
+
